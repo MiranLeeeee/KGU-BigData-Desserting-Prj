2c0f6293f01aed5ffb5de4cb7c4d1556e8512713
MiranLeeeee <miranleeeee@gmail.com>
2020-10-19 21:35:29 +0900

    load excel file

--- 4_UserInterface/action/DessertingController.java
+++ 4_UserInterface/action/DessertingController.java
@@ -57,4 +57,79 @@ public class DessertingController extends prj_DessertingController {
 	    return result;
 	  }
 
+    //각 토픽의 단어와 수치값 insert
+	@RequestMapping({"/desserting/putWords"})
+	@ResponseBody
+	public void putWords(HttpServletRequest request, @ModelAttribute DessertingForm form, ModelMap model) throws SQLException, IOException {
+		try {
+	            FileInputStream file = new FileInputStream("./Desserting/WEB-INF/excel/desserting/words.xlsx");
+	            XSSFWorkbook workbook = new XSSFWorkbook(file);
+
+	            int rowindex=0;
+	            int columnindex=0;
+	            XSSFSheet sheet=workbook.getSheetAt(0);
+	            int rows=sheet.getPhysicalNumberOfRows();
+
+	            for(rowindex=1;rowindex<rows;rowindex++){
+                    String value="";
+                    float num = -1;
+                    int topicNum = 0;
+	                XSSFRow row=sheet.getRow(rowindex);
+	                if(row !=null){
+	                    int cells=row.getPhysicalNumberOfCells();
+	                    for(columnindex=0; columnindex<=cells; columnindex++){
+	                        XSSFCell cell=row.getCell(columnindex);
+	                        if(cell==null) {
+	                            continue;
+	                        }else {
+	                           switch (cell.getCellTypeEnum()){
+	                            case FORMULA:
+	                                value=cell.getCellFormula();
+	                                break;
+	                            case NUMERIC:
+	                                num =(float)cell.getNumericCellValue();
+	                                break;
+	                            case STRING:
+	                                value=cell.getStringCellValue()+"";
+	                                break;
+	                            case BLANK:
+	                                value=cell.getBooleanCellValue()+"";
+	                                break;
+	                            case ERROR:
+	                                value=cell.getErrorCellValue()+"";
+	                                break;
+	                            }
+	                        }
+
+	                        if(num>0) {
+	                        	if(columnindex == 0) {
+	                        		topicNum = (int)num;
+	                        	}else {
+	                        		num = num;
+	                        	}
+	                        }else {
+	                        	value = value;
+	                        }
+	                        if(columnindex==2) {
+	                        	System.out.println(topicNum + "/"+ value+ "/" +num);
+	                        	getServiceManager().getDessertingCarrier().putWords(topicNum, value, num);
+	                        }
+	                    }
+	                }
+	            }
+		}catch(Exception e) {
+	            e.printStackTrace();
+	    }
+	}
+
+	  //각 토픽별 단어와 수치값 select
+	  @RequestMapping({"/desserting/getWords"})
+	  @ResponseBody
+	  public JSONArray getWords(HttpServletRequest request, @ModelAttribute DessertingForm form, ModelMap model) throws SQLException, IOException {
+		  int topicNum = form.getTopicNum();
+		  JSONArray result = new JSONArray();
+	      result = rows2JsonArray(getServiceManager().getDessertingCarrier().getWords(topicNum));
+
+	      return result;
+	  }
 }//DessertingController end
