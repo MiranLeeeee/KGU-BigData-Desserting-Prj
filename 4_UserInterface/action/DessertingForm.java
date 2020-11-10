5c8ef9e5e20ce8efb32827a137d7831a176a7691
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2020-11-10 22:43:11 +0900

    update form.java

--- 4_UserInterface/action/DessertingForm.java
+++ 4_UserInterface/action/DessertingForm.java
@@ -7,6 +7,11 @@ public class DessertingForm extends prj_DessertingForm{
 	private String name;
 	private String id;
 	private int topicNum;
+	private float t1;
+	private float t2;
+	private float t3;
+	private float t4;
+	private float t5;
 
 	public String getName() {
 		return name;
@@ -30,6 +35,46 @@ public class DessertingForm extends prj_DessertingForm{
 
 	public void setTopicNum(int topicNum) {
 		this.topicNum = topicNum;
+	}
+
+	public float getT1() {
+		return t1;
+	}
+
+	public void setT1(float t1) {
+		this.t1 = t1;
+	}
+
+	public float getT2() {
+		return t2;
+	}
+
+	public void setT2(float t2) {
+		this.t2 = t2;
+	}
+
+	public float getT3() {
+		return t3;
+	}
+
+	public void setT3(float t3) {
+		this.t3 = t3;
+	}
+
+	public float getT4() {
+		return t4;
+	}
+
+	public void setT4(float t4) {
+		this.t4 = t4;
+	}
+
+	public float getT5() {
+		return t5;
+	}
+
+	public void setT5(float t5) {
+		this.t5 = t5;
 	}
 
 }
